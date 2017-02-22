7f48662ce79dfc9fe1edfa9766cf5d6f83463763
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2017-02-22 01:32:30 -0300

    Criando o primeiro instalador do APP

--- app/main.js
+++ app/main.js
@@ -1,74 +1,106 @@
-// Handle Squirrel events for Windows immediately on start
-if (require('electron-squirrel-startup')) return;
+//handle setupevents as quickly as possible
+const setupEvents = require('./installers/setupEvents')
+if (setupEvents.handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
 
-const electron = require('electron');
-const {app, Menu} = electron;
-const {BrowserWindow} = electron;
-const {autoUpdater} = electron;
-const {ipcMain} = electron;
-const os = require('os');
+const electron = require('electron')
 
 const logger = require('winston');
 logger.level = 'debug';
 global.logger = logger;
 
-// Janela principal
-var mainWindow = null;
+// Module to control application life.
+const app = electron.app
+const {ipcMain} = require('electron')
+var path = require('path')
 
-// const template = [
-//     {
-//         role: 'window',
-//         submenu: [
-//             {
-//                 role: 'minimize'
-//             },
-//             {
-//                 role: 'close'
-//             }
-//         ]
-//     },
-//     {
-//         role: 'help',
-//         submenu: [
-//             {
-//                 label: 'Leia mais sobre',
-//                 click() { require('electron').shell.openExternal('http://natanielpaiva.github.io') }
-//             }
-//         ]
-//     }
-// ];
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+//Adds the main Menu to our app
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+let secondWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({titleBarStyle: 'hidden',
+    width: 1281,
+    height: 800,
+    minWidth: 1281,
+    minHeight: 800,
+    backgroundColor: '#312450',
+    show: false,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/views/pagina.html`)
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
 
 
-// const menu = Menu.buildFromTemplate(template)
-// Menu.setApplicationMenu(menu)
+  // Show the mainwindow when it is loaded and ready to show
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 
-// Quit when all windows are closed
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+  secondWindow = new BrowserWindow({frame: false,
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    backgroundColor: '#312450',
+    show: false,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+    parent: mainWindow
+  })
+
+  secondWindow.loadURL(`file://${__dirname}/views/pagina.html`)
+
+}
+
+ipcMain.on('open-second-window', (event, arg)=> {
+    secondWindow.show()
+})
+
+ipcMain.on('close-second-window', (event, arg)=> {
+    secondWindow.hide()
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    app.quit();
-});
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-// Criando a primeira janela da aplicação
-app.on('ready', function () {
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
 
-    logger.debug("Iniciando a aplicação");
-
-    // Criando a Janela principal
-    mainWindow = new BrowserWindow({
-        name: "indio",
-        width: 1000,
-        height: 800,
-        toolbar: false
-    });
-
-    // Arquivo HTML que vai abrir a primeira Janela
-    mainWindow.loadURL('file://' + __dirname + "/views/pagina.html");
-
-    // Já abre o web developer tools do google chromer
-    // mainWindow.webContents.openDevTools({detach:true});
-
-    // Fecha a janela
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
-
-});
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
