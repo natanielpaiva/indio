6b9207c48b2082047203bfd7a77177efa462b233
igorabpereira <igorabpereira@gmail.com>
2017-03-09 11:02:58 -0300

    Sqlite e outras maluqices. 1. Delete the node_modules directory. 2. Execute: npm cache clean 3. Re-install with: npm i

--- app/scripts/app/controllers/ConcessionariaController.js
+++ app/scripts/app/controllers/ConcessionariaController.js
@@ -1,76 +1,65 @@
+/* global myApp */
 
 myApp.controller('ConcessionariaController', function($scope, $q) {
+    $scope.concessionarias = [];
+    $scope.concessionaria = { nickname: "", bir:"" };
+    $scope.indexEditar = "";
 
+    var Datastore = require('nedb')
+        , db = new Datastore({ filename: 'db/concessionaria.db', autoload: true });
 
-        $scope.concessionarias = [];
-        $scope.concessionaria = { nickname: "", bir:"" };
-        $scope.indexEditar = "";
+    activate().then(function(response) {
+        $scope.concessionarias = response;
+    });
 
-        var Datastore = require('nedb')
-            , db = new Datastore({ filename: 'db/concessionaria.db', autoload: true });
-
-
-        // var usuario = new Datastore({ filename: 'db/usuario.json', autoload: true });
-
-        activate().then(function(response) {
-            $scope.concessionarias = response;
+    /**
+     * Iniciando os dados.
+     */
+    function activate() {
+        var deferred = $q.defer();
+        db.find({}, function(err, newDoc) {
+            deferred.resolve(newDoc);
         });
+        return deferred.promise;
+    }
 
-		/**
-		 * Iniciando os dados.
-		 */
-        function activate() {
-            var deferred = $q.defer();
-            db.find({}, function(err, newDoc) {
-                deferred.resolve(newDoc);
-            });
-            return deferred.promise;
-        }
-
-		/**
-		 * Adicionando .
-		 */
-        $scope.salvar = function() {
-
-            if ($scope.concessionaria.nickname !== "") {
-                if ($scope.concessionaria._id !== undefined && $scope.concessionaria._id !== "") {
-                    db.update({ _id: $scope.concessionaria._id },
+    /**
+     * Adicionando .
+     */
+    $scope.salvar = function() {
+        if ($scope.concessionaria.nickname !== "" && $scope.concessionaria.bir !== "") {
+            if (undefined != $scope.concessionaria._id) {
+                console.log('Achou id de ccs. UPDATE');
+                db.update({ _id: $scope.concessionaria._id },
                         {
-                            "nickname": $scope.concessionaria.nickname,
-                            "bir" :$scope.concessionaria.bir
+                            nickname: $scope.concessionaria.nickname,
+                            bir: $scope.concessionaria.bir
                         }, {}, function() {
                     });
+                $scope.concessionarias.push(angular.copy($scope.concessionaria));
+                $scope.concessionarias.splice($scope.indexEditar, 1);
+                $scope.concessionaria.nickname = "";
+                $scope.concessionaria.bir = "";
+            } else {
+                console.log('Não achou id de ccs. INSERT');
+                db.insert({ nickname: $scope.concessionaria.nickname, bir: $scope.concessionaria.bir }, function (err) {});
 
-                    $scope.concessionarias.push(angular.copy($scope.concessionaria));
-                    $scope.concessionarias.splice($scope.indexEditar, 1);
-                    $scope.concessionaria.nickname = "";
-                    $scope.concessionaria.bir = "";
-                } else {
-
-                    db.insert({
-                            "nickname": $scope.concessionaria.nickname,
-                            "bir": $scope.concessionaria.bir
-                        }, function(err, newDoc) {
-                    });
-
-                    $scope.concessionarias.push(angular.copy($scope.concessionaria));
-                    $scope.concessionaria.nickname = "";
-                    $scope.concessionaria.bir = "";
-                    delete $scope.concessionaria._id;
-                }
+                $scope.concessionarias.push(angular.copy($scope.concessionaria));
+                $scope.concessionaria.nickname = "";
+                $scope.concessionaria.bir = "";
+                delete $scope.concessionaria._id;
             }
+        }
+    };
 
-        };
+    $scope.editar = function(concessionaria, index) {
+        $scope.concessionaria = angular.copy(concessionaria);
+        $scope.indexEditar = index;
+    };
 
-        $scope.editar = function(concessionaria, index) {
-            $scope.concessionaria = angular.copy(concessionaria);
-            $scope.indexEditar = index;
-        };
-
-        $scope.deletar = function(_id, index) {
-            db.remove({ '_id': _id }, {}, function(err, numRemoved) {
-            });
-            $scope.concessionarias.splice(index, 1);
-        };        
-   });
+    $scope.deletar = function(_id, index) {
+        db.remove({ _id: _id }, {}, function(err, numRemoved) {});
+        $scope.concessionarias.splice(index, 1);
+    };        
+});
 
